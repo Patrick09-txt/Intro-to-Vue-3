4319bab433e98fafecbf9a3da92869ac466b05d2
Patrick Nyskjold <patr447g@edu.easj.dk>
2021-12-15 18:18:01 +0100

    List Rendering with sizes and details + id binding with variants

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const app = Vue.createApp({
             description: 'A placeholder description for Socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.zealand.dk/',
-            inventory: 0,
-            onSale: true
+            inStock: true,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: ['Small', 'Medium', 'Large', 'XL']
         }
     }
 })
