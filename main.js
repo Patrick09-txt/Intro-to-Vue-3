42af23de9e3fdafb26d5847296cfe97f898784a6
Patrick Nyskjold <patr447g@edu.easj.dk>
2021-12-15 18:28:11 +0100

    Event Handling with Add to Cart and Remove from Cart

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'A placeholder description for Socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.zealand.dk/',
+            // url: 'https://www.zealand.dk/',
             inStock: true,
+            // inventory: 8,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
             sizes: ['Small', 'Medium', 'Large', 'XL']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
