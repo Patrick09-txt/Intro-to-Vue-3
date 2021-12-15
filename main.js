ab42a2c853719dc473cabeb819786e2438ce7a90
Patrick Nyskjold <patr447g@edu.easj.dk>
2021-12-15 18:08:05 +0100

    Conditional Rendering with stock availability and sale status

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'A placeholder description for Socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.zealand.dk/'
+            url: 'https://www.zealand.dk/',
+            inventory: 0,
+            onSale: true
         }
     }
 })
